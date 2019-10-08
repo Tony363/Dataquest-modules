f33ba6710222e6703e59dd66eea75bc6fe72f960
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-10-08 19:16:22 +0800

    testing our ternary operators

--- datascience/teneraryOperators.py
+++ datascience/teneraryOperators.py
@@ -1,3 +1,4 @@
+## Lists
 content_ratings = {'4+': 4433, '9+': 987, '12+': 1155, '17+': 622}
 
 is_in_dictionary_1 = ('9+' in content_ratings)
@@ -9,5 +10,20 @@ is_in_dictionary_2 = (987 in content_ratings)
 
 # is_in_dictionary_3 = ('17+' in content_ratings)    
 # print(is_in_dictionary_3)
-# results = 'It exits' if '17+' in content_ratings else None
-print( 'It exists' if '17+' in content_ratings else None)
+results = 'It exits' if '17+' in content_ratings else None
+print('It exists' if '17+' in content_ratings else None)
+
+## Dictionaries
+content_ratings = {'4+': 4433, '12+': 1155, '9+': 987, '17+': 622}
+total_number_of_apps = 7197
+
+for ratings in content_ratings:
+    content_ratings[ratings] /= total_number_of_apps
+    content_ratings[ratings] *= 100
+percentage_17_plus = content_ratings['17+']
+
+import itertools
+
+percentage_15_allowed_dic = dict(itertools.islice(content_ratings.items(), 3))
+percentage_15_allowed = sum(percentage_15_allowed_dic.values())
+print(percentage_15_allowed)
