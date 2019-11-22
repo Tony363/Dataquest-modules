d4e3402e080c91d54af15635c781eb71283fe2e0
Tony Siu <48950649+thefool363@users.noreply.github.com>
2019-11-23 00:25:58 +0800

    hope this is about the thing he wants

--- datascience/formatting_csv.py
+++ datascience/formatting_csv.py
@@ -1,19 +1,47 @@
 import csv
 import os
-
-
+import sys
+import traceback
+import psutil
 
 new_format = []
 
 INPUT_DIR = r"C:\Users\Tony\Desktop\notpat"
 
+# def iterate_rows(infile,new_format):
+  
+    # try:
+    #     head = [int(next(infile))]
+    #     for row in head:
+    #         # print(row)
+    #         new_format.append(row)
+    #     # print(new_format)
+    # except MemoryError:
+
+    #     raise StopIteration()
+    
+
+     
 for filename in os.listdir(INPUT_DIR):
 
     with open(os.path.join(INPUT_DIR,filename), 'r+', encoding='utf-8') as infile:
-        head = [int(next(infile)) for row in range(2)]
+      
+        try:
+            while True:
+                try:
+                    head = [int(next(infile))]
+                    for row in head:
+                        # print(row)
+                        new_format.append(row)
+                    # print(new_format)
+                except MemoryError:
 
-        for row in head:
-            new_format.append(row)
+                    raise StopIteration()
+    
+        except StopIteration:
+            continue
+                
+                
 
 print(new_format)
 
