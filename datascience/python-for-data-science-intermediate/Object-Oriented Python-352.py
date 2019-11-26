ddc949062f02dd10cbde07f468da55fdadc0a233
Tony Siu <thefool363@gmail.com>
2019-11-26 15:54:55 +0800

    thats 2 courses done off dataquest

---
+++ datascience/python-for-data-science-intermediate/Object-Oriented Python-352.py
@@ -0,0 +1,129 @@
+## 1. Introduction ##
+
+l = [1, 2, 3]
+s = "string"
+d = {"a": 1, "b": 2}
+
+print(type(l))
+print(type(s))
+print(type(d))
+
+## 3. Defining a Class ##
+
+class NewList:
+    pass
+
+## 4. Instantiating a Class ##
+
+class NewList(DQ):
+    pass
+
+newlist_1 = NewList()
+print(type(newlist_1))
+    
+
+
+## 5. Creating Methods ##
+
+class NewList(DQ):
+    def first_method(self):
+        return "This is my first method"
+    
+newlist = NewList()
+  
+
+## 6. Understanding 'self' ##
+
+class NewList(DQ):
+    def first_method(self):
+        return "This is my first method"
+    def __str__(self):
+        return "{}".format(self.first_method)
+    
+newlist = NewList()
+result = newlist.first_method()
+print(result)
+# result()
+
+## 7. Creating a Method That Accepts an Argument ##
+
+class NewList(DQ):
+    def return_list(self,input_list):
+        
+        return input_list
+    
+newlist = NewList()
+result = newlist.return_list([1,2,3])
+    
+
+## 8. Attributes and the Init Method ##
+
+class NewList(DQ):
+    def __init__(self,data):
+        self.data = data
+
+my_list = NewList([1,2,3,4,5])
+print(my_list)
+
+## 9. Creating an Append Method ##
+
+# The NewList definition from the previous
+# screen is copied here for your convenience
+
+class NewList(DQ):
+    """
+    A Python list with some extras!
+    """
+    def __init__(self, initial_state):
+        self.data = initial_state
+     
+    def append(self,arg):
+        
+        self.data = self.data + [arg]
+
+my_list = NewList([1,2,3,4,5])
+print(my_list.data)
+my_list.append(6)
+print(my_list.data)
+
+x = [1,2,3,4,5] 
+y = [3,1,2,4]
+z = x + y
+print(z)
+    
+
+## 10. Creating and Updating an Attribute ##
+
+# The NewList definition from the previous
+# screen is copied here for your convenience
+
+class NewList(DQ):
+    """
+    A Python list with some extras!
+    """
+    def __init__(self, initial_state):
+        self.data = initial_state
+        self.calc_length()
+    
+    def calc_length(self):
+        """
+        A helper function to calculate the .length
+        attribute.
+        """
+        length = 0
+        for item in self.data:
+            length += 1
+        self.length = length
+    
+    def append(self, new_item):
+        """
+        Append `new_item` to the NewList
+        """
+        self.data = self.data + [new_item]
+        self.calc_length()
+
+fibonacci = NewList([1, 1, 2, 3, 5])
+print(fibonacci.length)
+
+fibonacci.append(8)
+print(fibonacci.length)
