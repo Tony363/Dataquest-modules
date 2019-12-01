9f247640470d1459618b1731ff8934ed54cfa62d
Tony Siu <thefool363@gmail.com>
2019-12-01 22:53:40 +0800

    different ways to index pandas data frame

--- datascience/formatting_csv.py
+++ datascience/formatting_csv.py
@@ -10,14 +10,14 @@ INPUT_DIR = r"C:\Users\Tony\Desktop\github\xgboost-with-raymond\excel"
 
 for filename in os.listdir(INPUT_DIR):
 
-    with open(os.path.join(INPUT_DIR,filename), 'r+') as infile:#encoding='utf-8'
+    with open(os.path.join(INPUT_DIR,filename), 'r+', encoding= 'utf-8-sig') as infile:#encoding='utf-8'
         
         head = csv.reader(infile)
 
         for row in head:
-            if 'ï»¿' in row:
-                continue
-            new_format.append(row[0])
+            # if 'ï»¿' in row:
+            #     continue
+            new_format.append(int(row[0]))
 
 print(new_format)
 
